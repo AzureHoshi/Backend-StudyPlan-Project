e3c4b12f372c18d82a2df6816731f38ad912619c
AzureHoshi <124436885+AzureHoshi@users.noreply.github.com>
2023-05-30 17:42:58 +0700

    ทำ path สำหรับ student feedback เพิ่ม: path /api/v1/getstatusfeedback สำหรับส่งค่า status และ function getStatusFeedback ที่ใช้ check ค่า และ ส่ง status เพิ่ม: path /api/v1/update_statusfeedback สำหรับ update ค่า statusFeedback ของนักศึกษาหลังจากที่ส่ง คำตอบของ feedback ไป ให้ status เป็น 1

--- respository/studyplan/Student.js
+++ respository/studyplan/Student.js
@@ -7,14 +7,14 @@ const env = require('../../env.js');
 const config = require('../../dbconfig.js')[env];
 const crud = require('./CrudModel.js');
 
-var table = 'student_feedback';
+var table = 'students';
 
 async function getFeedback(values) {
   var Query;
   var pool = mysql.createPool(config);
 
   return new Promise((resolve, reject) => {
-    Query = `INSERT INTO student_feedback (student_id, feedback_id, feedback_answer) VALUES ?`;
+    Query = `INSERT INTO students_feedback (student_id, feedback_id, sf_answer) VALUES ?`;
 
     console.log(Query);
 
@@ -33,6 +33,64 @@ async function getFeedback(values) {
   });
 }
 
+async function getStatusFeedback(stu_id) {
+  var Query;
+  var pool = mysql.createPool(config);
+
+  return new Promise((resolve, reject) => {
+    Query = `SELECT stu_status_feedback FROM ${table} WHERE stu_id = ${stu_id};`;
+
+    console.log(Query);
+
+    pool.query(Query, function (error, results1, fields) {
+      if (error) {
+        return resolve(reject(error));
+      }
+      if (results1.stu_status_feedback !== 1) {
+        return resolve({
+          statusCode: 200,
+          returnCode: 1,
+          message: `Get Student StatusFeedback successfully`,
+          statusFeedback: '1',
+        });
+      }
+      pool.end();
+      return resolve({
+        statusCode: 200,
+        returnCode: 1,
+        message: `Get Student StatusFeedback successfully`,
+        statusFeedback: '0',
+      });
+    });
+  });
+}
+
+async function updatedStatusFeedback(stu_id) {
+  var Query;
+  var pool = mysql.createPool(config);
+
+  return new Promise((resolve, reject) => {
+    Query = `UPDATE ${table} SET stu_status_feedback = 1 WHERE stu_id = ${stu_id}`;
+
+    console.log(Query);
+
+    pool.query(Query, function (error, results1, fields) {
+      if (error) {
+        return resolve(reject(error));
+      }
+      pool.end();
+      return resolve({
+        statusCode: 200,
+        returnCode: 1,
+        message: `Student Feedback inserted successfully`,
+        data: results1,
+      });
+    });
+  });
+}
+
 module.exports.StudentRepo = {
   getFeedback: getFeedback,
+  getStatusFeedback: getStatusFeedback,
+  updatedStatusFeedback: updatedStatusFeedback,
 };
--- routes/studyplans/FrontEndStudent.js
+++ routes/studyplans/FrontEndStudent.js
@@ -34,6 +34,7 @@ module.exports = (server) => {
     },
   });
 
+  // /api/v1/student_feedback
   server.route({
     method: 'POST',
     path: '/api/v1/student_feedback',
@@ -52,10 +53,78 @@ module.exports = (server) => {
         // body requests
         // const { student_id, feedback_id, feedback_answer } = request.payload;
         const data = request.payload; // รับค่าจาก body ของคำขอ
-        const values = data.map((item) => [item.student_id, item.feedback_id, item.feedback_answer]);
+        const values = data.map((item) => [item.student_id, item.feedback_id, item.sf_answer]);
         console.log(values);
 
         const responsedata = await student.StudentRepo.getFeedback(values);
+        if (responsedata.error) {
+          return responsedata.errMessage;
+        } else {
+          return responsedata;
+        }
+      } catch (err) {
+        server.log(['error', 'home'], err);
+        return err;
+      }
+    },
+  });
+
+  // /api/v1/getstatusfeedback
+  server.route({
+    method: 'POST',
+    path: '/api/v1/getstatusfeedback',
+    config: {
+      // config for multi body request
+      payload: {
+        multipart: true,
+      },
+      cors: {
+        origin: ['*'],
+        additionalHeaders: ['cache-control', 'x-requested-width'],
+      },
+    },
+    handler: async function (request, reply) {
+      try {
+        // body requests
+        // const { student_id, feedback_id, feedback_answer } = request.payload;
+        const { stu_id } = request.payload;
+
+        console.log(stu_id);
+        const responsedata = await student.StudentRepo.getStatusFeedback(stu_id);
+        if (responsedata.error) {
+          return responsedata.errMessage;
+        } else {
+          return responsedata;
+        }
+      } catch (err) {
+        server.log(['error', 'home'], err);
+        return err;
+      }
+    },
+  });
+
+  // /api/v1/update_statusfeedback
+  server.route({
+    method: 'POST',
+    path: '/api/v1/update_statusfeedback',
+    config: {
+      // config for multi body request
+      payload: {
+        multipart: true,
+      },
+      cors: {
+        origin: ['*'],
+        additionalHeaders: ['cache-control', 'x-requested-width'],
+      },
+    },
+    handler: async function (request, reply) {
+      try {
+        // body requests
+        // const { student_id, feedback_id, feedback_answer } = request.payload;
+        const { stu_id } = request.payload;
+
+        console.log(stu_id);
+        const responsedata = await student.StudentRepo.updatedStatusFeedback(stu_id);
         if (responsedata.error) {
           return responsedata.errMessage;
         } else {
