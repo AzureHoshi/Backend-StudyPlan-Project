b719f3a0c7ef3fe292b8d78a09b64c8df9a90740
AzureHoshi <124436885+AzureHoshi@users.noreply.github.com>
2023-06-02 14:19:10 +0700

    test

--- dbconfig.js
+++ dbconfig.js
@@ -7,23 +7,23 @@ var dbconfig = {
     password: 'rqzqrrk5it8ezefm',
     database: 'o0tkco8f80h07qtd',
   },
-  production: {
-    //connectionLimit : 10,
-    host: '128.199.188.223',
-    port: '4406',
-    user: 'root',
-    //// password: '1q2w3e4rP;;',
-    password: '1q2w3e4rP@ssw0rd',
-    database: '3cx-buzz',
-  },
-  local: {
-    //connectionLimit : 10,
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
-    password: '',
-    database: 'studyplan',
-  },
+  //   production: {
+  //     //connectionLimit : 10,
+  //     host: '128.199.188.223',
+  //     port: '4406',
+  //     user: 'root',
+  //     //// password: '1q2w3e4rP;;',
+  //     password: '1q2w3e4rP@ssw0rd',
+  //     database: '3cx-buzz',
+  //   },
+  //   local: {
+  //     //connectionLimit : 10,
+  //     host: 'localhost',
+  //     port: '3306',
+  //     user: 'root',
+  //     password: '',
+  //     database: 'studyplan',
+  //   },
 };
 
 module.exports = dbconfig;
