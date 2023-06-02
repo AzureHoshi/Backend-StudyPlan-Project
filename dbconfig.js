604fe4558a1f0f79ec43738da91e0557b3c205cd
AzureHoshi <124436885+AzureHoshi@users.noreply.github.com>
2023-06-02 14:31:09 +0700

    test3

--- dbconfig.js
+++ dbconfig.js
@@ -7,22 +7,22 @@ var dbconfig = {
     password: 'rqzqrrk5it8ezefm',
     database: 'o0tkco8f80h07qtd',
   },
-  //   production: {
-  //     //connectionLimit : 10,
-  //     host: 'lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  //     port: '3306',
-  //     user: 'dawthi1bsmaki7t6',
-  //     password: 'rqzqrrk5it8ezefm',
-  //     database: 'o0tkco8f80h07qtd',
-  //   },
-  //   local: {
-  //     //connectionLimit : 10,
-  //     host: 'lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  //     port: '3306',
-  //     user: 'dawthi1bsmaki7t6',
-  //     password: 'rqzqrrk5it8ezefm',
-  //     database: 'o0tkco8f80h07qtd',
-  //   },
+  production: {
+    //connectionLimit : 10,
+    host: 'lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    port: '3306',
+    user: 'dawthi1bsmaki7t6',
+    password: 'rqzqrrk5it8ezefm',
+    database: 'o0tkco8f80h07qtd',
+  },
+  local: {
+    //connectionLimit : 10,
+    host: 'lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    port: '3306',
+    user: 'dawthi1bsmaki7t6',
+    password: 'rqzqrrk5it8ezefm',
+    database: 'o0tkco8f80h07qtd',
+  },
 };
 
 module.exports = dbconfig;
