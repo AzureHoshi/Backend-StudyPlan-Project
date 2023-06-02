dfd36b3143dd757f5cfbe1cbc37a3b6b3225251b
AzureHoshi <124436885+AzureHoshi@users.noreply.github.com>
2023-06-02 14:26:18 +0700

    test1

--- dbconfig.js
+++ dbconfig.js
@@ -7,23 +7,22 @@ var dbconfig = {
     password: 'rqzqrrk5it8ezefm',
     database: 'o0tkco8f80h07qtd',
   },
-  //   production: {
-  //     //connectionLimit : 10,
-  //     host: '128.199.188.223',
-  //     port: '4406',
-  //     user: 'root',
-  //     //// password: '1q2w3e4rP;;',
-  //     password: '1q2w3e4rP@ssw0rd',
-  //     database: '3cx-buzz',
-  //   },
-  //   local: {
-  //     //connectionLimit : 10,
-  //     host: 'localhost',
-  //     port: '3306',
-  //     user: 'root',
-  //     password: '',
-  //     database: 'studyplan',
-  //   },
+  production: {
+    //connectionLimit : 10,
+    host: 'lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    port: '3306',
+    user: 'dawthi1bsmaki7t6',
+    password: 'rqzqrrk5it8ezefm',
+    database: 'o0tkco8f80h07qtd',
+  },
+  local: {
+    //connectionLimit : 10,
+    host: 'lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    port: '3306',
+    user: 'dawthi1bsmaki7t6',
+    password: 'rqzqrrk5it8ezefm',
+    database: 'o0tkco8f80h07qtd',
+  },
 };
 
 module.exports = dbconfig;
