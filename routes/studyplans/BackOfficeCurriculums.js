8cf90a3fe452c2ace45315885f88e0e6e0a82300
AzureHoshi <124436885+AzureHoshi@users.noreply.github.com>
2023-07-17 15:11:36 +0700

    test

--- routes/studyplans/BackOfficeCurriculums.js
+++ routes/studyplans/BackOfficeCurriculums.js
@@ -216,6 +216,7 @@ module.exports = (server) => {
         // body requests
         const { faculty_id } = request.payload;
         console.log('faculty_id: ', faculty_id);
+
         const responsedata = await Curriculum.CurriculumsRepo.getCurriculmByFaculty(faculty_id);
         if (responsedata.error) {
           return responsedata.errMessage;
